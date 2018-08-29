673714cadb25b11bc7e77e2323b8bb9a43a99597
YangKui <c6swift@gmail.com>
2018-08-30 00:55:57 +0800

    config: init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { createResource } from 'simple-cache-provider';
+
+console.log(createResource);
+
+class App extends Component {
+  render() {
+    return <div>skr</div>;
+  }
+}
+
+export default App;
