a16c23e60ebc4c8829d39af14bbcbe3fbd18f8ca
YangKui <c6swift@gmail.com>
2018-08-30 03:18:23 +0800

    feat: 支持点击 marker 展示 info window

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,24 +4,46 @@ import {
   Marker,
   withGoogleMap,
   withScriptjs,
+  InfoWindow,
 } from 'react-google-maps';
-import { withProps, compose } from 'recompose';
+import { withProps, compose, withStateHandlers } from 'recompose';
 
 const API_KEY = 'AIzaSyCoJc7_HdAr12c7DDQ1VRqdPWbFsJDJuww';
 
-const Map = ({ center = { lat: 59.95, lng: 30.33 }, zoom = 13 }) => (
+const Map = ({
+  center = { lat: 59.95, lng: 30.33 },
+  zoom = 13,
+  isOpen,
+  onToggleOpen,
+}) => (
   <div style={{ height: '100vh', width: '100%' }}>
     <GoogleMap
       bootstrapURLKeys={{ key: API_KEY }}
       defaultCenter={center}
       defaultZoom={zoom}
     >
-      <Marker position={{ lat: 58.34, lng: 32.1 }} />
+      <Marker position={{ lat: 59.34, lng: 30.1 }} onClick={onToggleOpen}>
+        {isOpen && (
+          <InfoWindow onCloseClick={onToggleOpen}>
+            <p>this is a thing</p>
+          </InfoWindow>
+        )}
+      </Marker>
     </GoogleMap>
   </div>
 );
 
 export default compose(
+  withStateHandlers(
+    {
+      isOpen: false,
+    },
+    {
+      onToggleOpen: ({ isOpen }) => () => ({
+        isOpen: !isOpen,
+      }),
+    }
+  ),
   withProps({
     containerElement: <div style={{ height: '100vh', width: '100%' }} />,
     loadingElement: <div style={{ height: `100%` }} />,
