8dd1d2d0c29ff70663e462bbae3519fd3829b8dd
YangKui <c6swift@gmail.com>
2018-09-13 00:59:41 +0800

    feat: 添加 nearby API

--- src/api/geocode.js
+++ src/api/geocode.js
@@ -1,9 +1,40 @@
 import { API_KEY } from '../constant';
+
+const getAPIUrl = ({ category, action, ...params }) =>
+  `https://maps.googleapis.com/maps/api/${category}/${
+    action ? action + '/' : ''
+  }json?${Object.entries(params)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&')}&key=${API_KEY}`;
+
 export const getLocationWithLatLng = async ({ lat, lng }) => {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${API_KEY}`;
+  if (!(lat && lng)) return;
+  const url = getAPIUrl({ category: 'geocode', latlng: `${lat},${lng}` });
 
   const res = await fetch(url, {
     mode: 'cors',
+  });
+  if (res.ok) {
+    return res.json();
+  } else {
+    return res.statusText;
+  }
+};
+
+export const getPlacesWithKeyword = async ({ lat, lng }) => {
+  if (!(lat && lng)) return;
+  const location = `${lat},${lng}`;
+  const url = getAPIUrl({
+    category: 'place',
+    action: 'nearbysearch',
+    location,
+    radius: 10000,
+  });
+  const res = await fetch(url, {
+    mode: 'cors',
+    headers: {
+      'content-type': 'application/json',
+    },
   });
   if (res.ok) {
     return res.json();
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { createResource } from 'simple-cache-provider';
 import { cache } from '../cache';
-import { getLocationWithLatLng } from '../api/geocode';
+import { getLocationWithLatLng, getPlacesWithKeyword } from '../api/geocode';
 
-const locationResource = createResource(
-  getLocationWithLatLng,
+const nearbyResource = createResource(
+  getPlacesWithKeyword,
   ({ lng, lat }) => `${lat}${lng}`
 );
 
-const getLocation = center => locationResource.read(cache, center);
+const getNearby = center => nearbyResource.read(cache, center);
 const Location = ({ center }) => {
   if (!center) return null;
-  const res = getLocation(center);
-  return <div>{JSON.stringify(res)}</div>;
+  try {
+    const res = getNearby(center);
+    return <div>{JSON.stringify(res)}</div>;
+  } catch (err) {
+    return <h1>error</h1>;
+  }
 };
 
 export default Location;
