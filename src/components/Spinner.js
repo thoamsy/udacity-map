b85d8d2533b9493911f2af55c94ae9c95eb34adb
YangKui <c6swift@gmail.com>
2018-10-02 21:35:00 +0800

    refactor: 使用 styled-component 重构 Spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Spinner.css';
+import styled, { keyframes, css } from 'styled-components';
 
 const SPINNER_SIZES = {
   small: 30,
@@ -13,39 +13,94 @@ const STROKE_WIDTHS = {
   large: 6,
 };
 
-const PATH_CLASS_NAMES = {
-  small: 'SmallSpinnerPath',
-  medium: 'MediumSpinnerPath',
-  large: 'LargeSpinnerPath',
+const DASHS = {
+  small: 100,
+  medium: 150,
+  large: 200,
 };
 
+const rotator = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(270deg);
+  }
+`;
+const rotatorRule = css`
+  ${rotator} 1.4s linear infinite;
+`;
+
+const colors = keyframes`
+  0% { stroke: #4285F4; }
+	25% { stroke: #DE3E35; }
+	50% { stroke: #F7C223; }
+	75% { stroke: #1B9A59; }
+  100% { stroke: #4285F4; }
+`;
+const circleRules = Object.entries(DASHS).reduce((result, [size, offset]) => {
+  const rule = keyframes`
+ 0% { stroke-dashoffset: ${offset}; }
+ 50% {
+   stroke-dashoffset: ${offset / 4};
+   transform:rotate(135deg);
+ }
+ 100% {
+   stroke-dashoffset: ${offset};
+   transform:rotate(450deg);
+ }
+`;
+  result[size] = css`
+ ${rule} 1.4s ease-in-out infinite,
+  ${colors} 5.6s ease-in-out infinite;
+    `;
+  return result;
+}, {});
+console.log(circleRules);
+
+const centerRule = css`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+const SpinnerContainer = styled.svg`
+  animation: ${rotatorRule};
+  ${({ size }) => (size === 'large' ? centerRule : '')};
+`;
+
+const Circle = styled.circle`
+  transform-origin: center;
+  stroke-dasharray: ${props => DASHS[props.size]};
+  stroke-dashoffset: 0;
+  animation: ${props => circleRules[props.size]};
+`;
+
 // Heavily inspired by https://codepen.io/mrrocks/pen/EiplA
-export default function Spinner({size = 'small'}) {
+export default function Spinner({ size = 'small' }) {
   const baseSize = SPINNER_SIZES[size];
   const pathSize = baseSize / 2;
   const strokeWidth = STROKE_WIDTHS[size];
   const pathRadius = `${baseSize / 2 - strokeWidth}px`;
-  const className = PATH_CLASS_NAMES[size];
-  const containerClassName = `SpinnerContainer SpinnerContainer-${size}`;
 
   return (
-    <div className={containerClassName}>
-      <svg
-        className={className}
-        width={baseSize}
-        height={baseSize}
-        viewBox={`0 0 ${baseSize} ${baseSize}`}>
-        <circle
-          className="SpinnerPath"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth={strokeWidth}
-          strokeLinecap="round"
-          cx={pathSize}
-          cy={pathSize}
-          r={pathRadius}
-        />
-      </svg>
-    </div>
+    <SpinnerContainer
+      size={size}
+      width={baseSize}
+      height={baseSize}
+      viewBox={`0 0 ${baseSize} ${baseSize}`}
+    >
+      <Circle
+        size={size}
+        className="SpinnerPath"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        cx={pathSize}
+        cy={pathSize}
+        r={pathRadius}
+      />
+    </SpinnerContainer>
   );
 }
