229d46173db48767f56934661d1ad2821183527c
YangKui <c6swift@gmail.com>
2018-11-04 10:57:05 +0800

    feat: 通过 tab 切换 place item 后，可以使用 space 或者 enter 直接选择

--- src/components/PlaceList.jsx
+++ src/components/PlaceList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { unstable_createResource as createResource } from 'react-cache';
 import styled from 'styled-components';
 
@@ -49,10 +49,35 @@ const PlaceList = ({ active }) => {
   };
 
   const places = useNearBy({ center, keyword, dispatch });
+  const ref = useRef();
+  // ref.current?.focus?.();
+  ref.current && ref.current.focus();
+
+  const [currentCursor, setCursor] = useState(0);
+  const moveCursor = event => {
+    event.preventDefault();
+    const {
+      key,
+      target: { dataset },
+    } = event;
+    if (key === ' ' || key === 'Enter') {
+      onClickPlace(dataset.id)();
+      setCursor(dataset.index);
+    }
+  };
+
   return (
-    <ul className="menu-list" role="menu">
+    <ul className="menu-list" role="menu" onKeyPress={moveCursor}>
       {places.map((place, i) => (
-        <li key={place.id} onClick={onClickPlace(place.id)}>
+        <li
+          key={place.id}
+          ref={i === currentCursor ? ref : null}
+          onClick={onClickPlace(place.id)}
+          data-id={place.id}
+          data-index={i}
+          tabIndex={0}
+          role="menuitem"
+        >
           <Place
             isActive={active === (place.id ?? i)}
             className="has-text-light"
